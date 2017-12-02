3f65b91e271236062890753e3d213d339946aa02
JJCid <josejaviercidpinel@gmail.com>
2017-12-02 18:14:37 +0100

    fin 08-directivas_personalizadas

--- 08-directivas_personalizadas/src/App.vue
+++ 08-directivas_personalizadas/src/App.vue
@@ -2,13 +2,35 @@
   <div class="container">
     <div class="jumbotron">
       <span v-decorar:grande.bold.italic="{familia: 'times new roman', color: 'red'}">Aprende Vue.js</span>
+      <hr>
+      <span v-decorar-local:pequeno="{familia: 'sheriff', color: 'green'}">Aprende Vue.js</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  directives: {
+    'decorar-local' : {
+      bind(el, binding, vnode) {
+        el.style.fontFamily = binding.value.familia;
+        el.style.color = binding.value.color;
+
+        if(binding.arg == 'grande'){
+          el.style.fontSize = '50px';
+        }
+        if(binding.arg == 'pequeno'){
+          el.style.fontSize = '15px';
+        }
+        if(binding.modifiers['bold']){
+          el.style.fontWeight = 'bold';
+        }
+        if(binding.modifiers['italic']){
+          el.style.fontStyle = 'italic';
+        }
+      }
+    }
+  }
 }
 </script>
 

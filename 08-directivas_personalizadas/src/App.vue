a4fd0be5d0c332967b90ebaf89538e3287b9214b
JJCid <josejaviercidpinel@gmail.com>
2017-12-02 18:04:12 +0100

    08-directivas_personalizadas primera parte

---
+++ 08-directivas_personalizadas/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <span v-decorar:grande.bold.italic="'times new roman'">Aprende Vue.js</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ 08-directivas_personalizadas/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('decorar',{
+  bind(el, binding, vnode) {
+    el.style.fontFamily = binding.value;
+
+    if(binding.arg == 'grande'){
+      el.style.fontSize = '50px';
+    }
+    if(binding.modifiers['bold']){
+      el.style.fontWeight = 'bold';
+    }
+    if(binding.modifiers['italic']){
+      el.style.fontStyle = 'italic';
+    }
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})

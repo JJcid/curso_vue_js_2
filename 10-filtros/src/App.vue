6348818ca18b19f0415b49c817822ba00b0a4cdb
JJCid <josejaviercidpinel@gmail.com>
2017-12-02 20:06:34 +0100

    fin 10-filtros

--- 10-filtros/src/App.vue
+++ 10-filtros/src/App.vue
@@ -1,60 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="jumbotron">
+      <span> {{ mensaje | mayusculas }} </span> <!-- | indicativo que vamos a aplicar un filtro -->
+      <hr>
+      <span> {{ mensaje | suspensivos }} </span> <!-- filtro global en main.js -->
+      <hr>
+      <span> {{ mensaje | suspensivos | mayusculas }} </span> <!-- ambos filtros -->
+      <hr>
+      <span> {{ mensaje }} </span> <!-- sin filtro -->
+      <hr>
+      <span> {{ temperatura }} Cº</span>
+      <hr>
+      <span> {{ temperatura | aFahrenheit }} F </span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      mensaje: 'Aprende vue.js',
+      temperatura: 30
+    }
+  },
+  filters: {
+    mayusculas(mensaje) {
+      return mensaje.toUpperCase();
+    },
+    aFahrenheit(temperatura){
+      return (9/5 * temperatura) + 32;
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- 10-filtros/src/main.js
+++ 10-filtros/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('suspensivos', function(texto){
+  return texto.substring(0,10) + '...';
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
